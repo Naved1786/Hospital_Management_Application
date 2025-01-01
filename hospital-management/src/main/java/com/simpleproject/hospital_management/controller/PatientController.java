aa6e3b502c30772ca4c13e6e0734761b8c783d2c
Navedul Hasan <navedmalik092@gmail.com>
2025-01-01 19:08:53 +0530

    save and get APIs for the Patients

--- hospital-management/src/main/java/com/simpleproject/hospital_management/controller/PatientController.java
+++ hospital-management/src/main/java/com/simpleproject/hospital_management/controller/PatientController.java
@@ -1,6 +1,30 @@
 package com.simpleproject.hospital_management.controller;
 
+
+import com.simpleproject.hospital_management.model.Patient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/patient/apis")
 public class PatientController {
     // Assignment
+    HashMap<Integer, Patient> patientDb=new HashMap<>();
+    @PostMapping("/save")
+    public String savePatient(@RequestBody Patient inputPatientRequest){
+        patientDb.put(inputPatientRequest.getId(),inputPatientRequest);
+        return "patient save successfully";
+    }
 
+    @GetMapping("/findAll")
+    public HashMap<Integer, Patient> findAllPatient(){
+        return patientDb;
+    }
+
+    @GetMapping("/find/{id}")
+    public Patient findPatientById(@PathVariable int id){
+        Patient patient=patientDb.get(id);
+        return  patient;
+    }
 }
--- hospital-management/src/main/java/com/simpleproject/hospital_management/model/Patient.java
+++ hospital-management/src/main/java/com/simpleproject/hospital_management/model/Patient.java
@@ -1,5 +1,58 @@
 package com.simpleproject.hospital_management.model;
 
 public class Patient {
+    private int id;
+    private String name;
+    private String gender;
+    private int age;
+    private String address;
+    private String contact;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 }
